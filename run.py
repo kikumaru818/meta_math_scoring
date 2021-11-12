1f94a4ee14dafcd361a8701f0262dfdda2685f2d
Aritra Ghosh <aritraghosh.iem@gmail.com>
2021-11-11 20:03:22 -0500

    tasks run

--- run.py
+++ run.py
@@ -45,14 +45,14 @@ def is_long(combo):
 
 save = False
 hyperparameters = [
-    [('task',), tasks[8:12]],#[  "Grade 4/2017_DBA_DR04_1715RE1T10_05"]],#, 'facebook/bart-large','microsoft/deberta-v2-xlarge', 'facebook/bart-large'
+    [('task',), tasks[8:16]],#[  "Grade 4/2017_DBA_DR04_1715RE1T10_05"]],#, 'facebook/bart-large','microsoft/deberta-v2-xlarge', 'facebook/bart-large'
     [('lm',), ['bert-base-uncased']],#'bert-base-uncased','roberta-base','bert-large-uncased','roberta-large','gpt2'
     # [('lm',), ['gpt2']],#'bert-base-uncased','roberta-base','bert-large-uncased','roberta-large','gpt2'
     # [('losses',), ['cce;qwp', 'cce', 'qwp']],
     [('losses',), [ 'cce' ]],
     [('generate',), ['none']],
-    [('lr',), [2e-5]],#2e-4
-    [('iters',), [50]],
+    [('lr',), [1e-4]],#2e-4
+    [('iters',), [20]],
     [('seed',), [999]],
     [('batch_size',), [32]],
 ]
