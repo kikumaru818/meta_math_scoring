79aa0da1a161ec9dbf860940993a91b7eff7f240
Aritra Ghosh <aritraghosh.iem@gmail.com>
2021-11-12 23:04:01 -0500

    run

--- run.py
+++ run.py
@@ -46,7 +46,7 @@ def is_long(combo):
 save = False
 fixed_params = '   '.join(['--neptune', '--cuda', '--include_passage'])
 hyperparameters = [
-    [('task',), tasks[:8]],#[  "Grade 4/2017_DBA_DR04_1715RE1T10_05"]],#, 'facebook/bart-large','microsoft/deberta-v2-xlarge', 'facebook/bart-large'
+    [('task',), tasks[8:12]],#[  "Grade 4/2017_DBA_DR04_1715RE1T10_05"]],#, 'facebook/bart-large','microsoft/deberta-v2-xlarge', 'facebook/bart-large'
     [('lm',), ['bert-base-uncased']],#'bert-base-uncased','roberta-base','bert-large-uncased','roberta-large','gpt2'
     # [('lm',), ['gpt2']],#'bert-base-uncased','roberta-base','bert-large-uncased','roberta-large','gpt2'
     # [('losses',), ['cce;qwp', 'cce', 'qwp']],
